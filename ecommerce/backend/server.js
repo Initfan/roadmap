b435c6682e28010e80ae6c65e49e0cc4793873fd
irfan rifani <feeinitfan@gmail.com>
2024-06-17 00:31:56 +0700

    Error middleware

--- ecommerce/backend/server.js
+++ ecommerce/backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js'
 const app = express()
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
@@ -13,5 +14,8 @@ connectDB();
 app.get('/', (req, res) => res.send('API is running...'))
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
