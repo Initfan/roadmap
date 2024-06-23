c61db1e30f2b54187843cb4dd7734cc257925600
irfan rifani <feeinitfan@gmail.com>
2024-06-23 13:24:21 +0700

    invesment app

--- invesment/src/App.jsx
+++ invesment/src/App.jsx
@@ -21,6 +21,7 @@ function App() {
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
+      {/* <Results input={userInput} /> */}
       {!inputIsValid && <p className="center">Please enter a duration greater than zero</p>}
       {inputIsValid && <Results input={userInput} />}
     </>
