56d4186ae2ca7e36d1d6eae90033243c7343cf25
irfan rifani <feeinitfan@gmail.com>
2024-06-25 19:11:14 +0700

    fetching data

--- section-15-http/src/components/AvailablePlaces.jsx
+++ section-15-http/src/components/AvailablePlaces.jsx
@@ -1,10 +1,24 @@
+import React, { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([])
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      const res = await fetch('http://localhost:3000/places')
+      const data = await res.json()
+      setAvailablePlaces(data.places)
+    }
+    fetchPlaces()
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
+      isLoading={true}
+      loadingText='Fetching place data...'
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
