17206b26167f8a8f8d56f934506b0c19c913e9df
irfan rifani <feeinitfan@gmail.com>
2024-06-29 16:20:00 +0700

    react testing using jest

---
+++ section-29-testing/src/components/Greeting.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import Output from './Output';
+
+const Greeting = () => {
+    const [changedText, setChangedText] = useState(false);
+
+    const changeTextHandler = () => {
+        setChangedText(true);
+    }
+
+    return (
+        <>
+            <h2>Hello World!</h2>
+            {!changedText
+                ? <Output>It's good to see you</Output>
+                : <Output>Changed!</Output>
+            }
+            <button onClick={changeTextHandler}>Change Text!</button>
+        </>
+    )
+}
+
+export default Greeting
